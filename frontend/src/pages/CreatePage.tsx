fcfbe8d3153a72b939b1c567f353502181b8e58f
Henry <henry3399@hotmail.co.nz>
2023-07-08 20:20:27 +1200

    Added all features

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -1,38 +1,57 @@
-import {useState} from "react";
+import { useState } from "react";
 
-import { IonInput, IonPage, IonText } from "@ionic/react";
+import { IonInput, IonPage, IonText, IonTextarea } from "@ionic/react";
 import HeaderBar from "../components/HeaderBar";
 
 const CreatePage = () => {
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
 
-    return (
-        <div>
-            <IonPage>
-                <HeaderBar />
-                <div className="text-xl text-center">
-                    Add a new place of interest
-                </div>
-                <div className="border-2">
-                    Add Image
-                    <input type="file" />
-                </div>
-
-                <IonInput label="Name" placeholder="Enter the name of the POI..."/>
-                <IonInput label="Description" placeholder="Enter description..."/>
-
-                <div>
-                    Add audio file
-                    <input type="file" />
-                </div>
-
-                <button type="button">Submit</button>
-
-                
-            </IonPage>
+  return (
+    <div>
+      <HeaderBar />
+      <div className="border-2 space-y-10">
+        <div className="text-xl text-center">Add a new place of interest</div>
+        <div className="border-2">
+          Add Image <br />
+          <input type="file" />
         </div>
-    )
-}
+
+        <div>
+          <label htmlFor="name">Name </label>
+          <br />
+          <input
+            id="name"
+            className="border-2"
+            type="text"
+            onChange={(e) => setName(String(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="description">Description </label>
+          <br />
+          <input
+            id="description"
+            className="border-2 w-full"
+            type="text"
+            onChange={(e) => setDescription(String(e.target.value))}
+          />
+        </div>
+
+        <div className="border-2">
+          Add audio file <br />
+          <input type="file" />
+        </div>
+
+        <div className="m-auto w-1/2 border-2">
+          <button type="button" className="border-2 w-full">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CreatePage;
