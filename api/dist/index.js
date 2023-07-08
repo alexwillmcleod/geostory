7ef87461ed8f91720dca4eb9e3b537d55b7adff7
Alex McLeod <alexwillmcleod@gmail.com>
2023-07-08 12:52:34 +1200

    feat: setup basic express api

--- api/dist/index.js
+++ api/dist/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var dotenv_1 = require("dotenv");
+(0, dotenv_1.config)(); // Import environment variables from config
+var app = (0, express_1.default)();
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Listening on port ".concat(port));
+});
--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+import express from 'express';
+import { config } from 'dotenv';
+
+config(); // Import environment variables from config
+
+const app = express();
+app.get('/', async (req, res) => {
+  return res.status(200).send('Hello, World!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
