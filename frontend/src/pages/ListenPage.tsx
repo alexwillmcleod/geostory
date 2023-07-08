3cbcef5d6fdfe471df852acc20545d2c035e70d1
henrym25 <henry3399@hotmail.co.nz>
2023-07-09 01:18:15 +1200

    Added temp Audio Player

--- frontend/src/pages/ListenPage.tsx
+++ frontend/src/pages/ListenPage.tsx
@@ -1,4 +1,8 @@
+import sound from "./test2.mp3";
+
 const ListenPage = () => {
+  const url = "http://streaming.tdiradio.com:8000/house.mp3"; //Just a sample URL from the internet
+
   return (
     <div className="border-2 border-red-400 h-full text-center flex-1">
       <div className="h-2/5 border-blue-400 border-2">Cover image</div>
@@ -6,9 +10,10 @@ const ListenPage = () => {
         <span className="text-3xl">Name</span>
         <div>Play</div>
         {/* <div>Back</div> */}
-        <audio controls src="test.mp3">
-          test
+        <audio controls>
+          <source src={url} type="audio/mpeg" />
         </audio>
+        {/* <ReactAudioPlayer src=tes/> */}
         <button className="border-2 w-1/2 rounded-xl m-auto justify-center h-14">
           Back to Scanner
         </button>
