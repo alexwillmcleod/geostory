563c27817f4f62398437fcf1f7e65b66986d2d2b
Henry <henry3399@hotmail.co.nz>
2023-07-08 16:17:15 +1200

    "Added base headerbar component"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { IonApp } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
+import Page from './pages/Page'
+import { Route } from 'react-router'
 
 function App() {
   return (
     <IonApp>
       <IonReactRouter>
+        <Route path="/" component={Page} />
       </IonReactRouter>
     </IonApp>
   )
