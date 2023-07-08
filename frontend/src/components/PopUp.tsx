f5d9450d9b91ab2599985838799d6bdf55b6e3bb
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-08 22:17:43 +1200

    updated PopUp layout

--- frontend/src/components/PopUp.tsx
+++ frontend/src/components/PopUp.tsx
@@ -5,12 +5,19 @@ interface PopUpProps {
   onClose: () => void;
 }
 
-const PopUp: React.FC<PopUpProps> = ({ data, onClose }) => {
+const PopUp: React.FC<PopUpProps> = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-8 rounded-lg">
-        <h2 className="text-2xl font-bold mb-4">Pop Up Title</h2>
-        <p className="text-gray-700">{data}</p>
+      <div className="bg-white p-8 rounded-lg w-96">
+        <div className="flex items-center justify-center h-40">
+          <img
+            src="image-placeholder.jpg" // Replace with your image source
+            alt="Image Placeholder"
+            className="object-contain h-full"
+          />
+        </div>
+        <h2 className="text-2xl font-bold mt-4">Pop Up Title</h2>
+        <p className="text-gray-700 text-sm mt-2">Description</p>
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
           onClick={onClose}
