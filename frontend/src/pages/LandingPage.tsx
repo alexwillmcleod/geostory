9e7bd69163cab75b1f525112e038b64a0794531c
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-08 17:13:58 +1200

    qrcodescranner + header

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,11 +1,16 @@
 import { QrScanner } from '@yudiel/react-qr-scanner';
+import HeaderBar from '../components/HeaderBar';
 
 const LandingPage = () => {
   return (
+    <div>
+    <HeaderBar></HeaderBar>
     <QrScanner
       onDecode={(result) => console.log(result)}
       onError={(error) => console.log(error?.message)}
     />
+    </div>
+    
   );
 }
 
