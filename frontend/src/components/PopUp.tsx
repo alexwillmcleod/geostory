6127a123eef71117fc6d6d6d5f635921b1c474ed
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-08 21:38:00 +1200

    pop up modal

--- frontend/src/components/PopUp.tsx
+++ frontend/src/components/PopUp.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 interface PopUpProps {
-  data: string;
   onClose: () => void;
 }
 
-const PopUp: React.FC<PopUpProps> = ({ data, onClose }) => {
+const PopUp: React.FC<PopUpProps> = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-8 rounded-lg">
         <h2 className="text-2xl font-bold mb-4">Pop Up Title</h2>
-        <p className="text-gray-700">{data}</p>
+        <p className="text-gray-700">Pop Up Content</p>
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
           onClick={onClose}
