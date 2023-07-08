76a2fc5b6f0ea2d0115b7d367d0a16e588468ac6
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-08 22:59:47 +1200

    updated placeholder info (2)

--- frontend/src/components/PopUp.tsx
+++ frontend/src/components/PopUp.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeadphones } from '@fortawesome/free-solid-svg-icons';
 
 interface PopUpProps {
   data: string;
@@ -11,18 +13,21 @@ const PopUp: React.FC<PopUpProps> = ({ onClose }) => {
       <div className="bg-white p-8 rounded-lg w-96">
         <div className="flex items-center justify-center h-40">
           <img
-            src="../assets/PlaceholderCoverImage.png" // Replace with your image source
+            src="https://imgur.com/0Ns3zb2.jpeg"
             alt="Image Placeholder"
             className="object-contain h-full"
           />
         </div>
         <h2 className="text-2xl font-bold mt-4">The Grass Touched By Backstreet Boys</h2>
-        <p className="text-gray-700 text-sm mt-2">Amidst their virtual realm, four secluded students stumbled upon an extraordinary secret hidden within the blades of grass, forever altering their perception of the digital world.</p>
+        <p className="text-gray-700 text-sm mt-2">
+          Amidst their virtual realm, four secluded students stumbled upon an extraordinary secret hidden within the blades of grass, forever altering their perception of the digital world.
+        </p>
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
           onClick={onClose}
         >
-          Close
+          <FontAwesomeIcon icon={faHeadphones} className="mr-2" />
+          Listen
         </button>
       </div>
     </div>
