112a69082e4788f917d74d2c5ac6260ec1be2e8e
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-08 21:55:13 +1200

    qrtest popup 3

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -7,37 +7,29 @@ import PopUp from '../components/PopUp';
 interface LandingPageProps extends RouteComponentProps {}
 
 const LandingPage: React.FC<LandingPageProps> = () => {
-  const [showPopUp, setShowPopUp] = useState(false);
-  const [qrCodeData, setQRCodeData] = useState('');
-
-  const handleOpenPopUp = (data: string) => {
-    setQRCodeData(data);
-    setShowPopUp(true);
-  };
-
-  const handleClosePopUp = () => {
-    setShowPopUp(false);
-  };
+  const [qrCodeData, setQRCodeData] = useState<string | null>(null);
 
   const handleQRCodeDecode = (result: string | null) => {
-    if (result) {
-      handleOpenPopUp(result);
-    }
+    setQRCodeData(result);
+  };
+
+  const handlePopUpClose = () => {
+    setQRCodeData(null);
   };
 
   return (
     <div>
       <HeaderBar />
       <div className="flex items-center justify-center h-screen">
-        {!showPopUp && (
+        {qrCodeData === null ? (
           <QrScanner
             onDecode={handleQRCodeDecode}
             onError={(error) => console.log(error?.message)}
           />
+        ) : (
+          <PopUp data={qrCodeData} onClose={handlePopUpClose} />
         )}
       </div>
-
-      {showPopUp && <PopUp data={qrCodeData} onClose={handleClosePopUp} />}
     </div>
   );
 };
