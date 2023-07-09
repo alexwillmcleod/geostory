e71a6c28a36b8ce6cab0779b154c07f2d4392a90
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-09 15:28:59 +1200

    updated Create Page

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { FaUpload } from "react-icons/fa";
+import QrPopUp from "../components/QrCodePopUp";
 
 const CreatePage = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [uploadedImage, setUploadedImage] = useState<null | string>(null);
+  const [selectedAudio, setSelectedAudio] = useState<null | File>(null);
+  const [showPopUp, setShowPopUp] = useState(false); // State for controlling the pop-up
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -14,38 +17,61 @@ const CreatePage = () => {
     }
   };
 
+  const handleAudioUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSelectedAudio(file);
+    }
+  };
+
+  const handleSubmit = () => {
+    // Perform submission logic here
+    // Show the pop-up
+    setShowPopUp(true);
+  };
+
+  const handleClosePopUp = () => {
+    setShowPopUp(false);
+  };
+
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="text-4xl text-center mb-8 font-bold">Create GeoStory</div>
       <div className="w-full max-w-md px-4 mb-8">
-        <div className="border-2 rounded-lg p-8 mb-8 flex flex-col items-center">
-          <label
-            htmlFor="imageUpload"
-            className="cursor-pointer flex flex-col items-center w-full"
-          >
-            <div className="w-full h-24 mb-2 relative">
-              {uploadedImage ? (
-                <img
-                  className="w-full h-full object-cover rounded-lg"
-                  src={uploadedImage}
-                  alt="Uploaded Cover"
-                />
-              ) : (
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <FaUpload className="text-3xl" />
-                </div>
-              )}
+        <div className="border-2 rounded-lg mb-8">
+          {uploadedImage ? (
+            <img
+              className="w-full h-full object-cover rounded-lg"
+              src={uploadedImage}
+              alt="Uploaded Cover"
+            />
+          ) : (
+            <div className="w-full h-32 mb-2 border border-dashed border-gray-400 flex justify-center items-center">
+              <label
+                htmlFor="imageUpload"
+                className="cursor-pointer flex flex-col items-center"
+              >
+                <FaUpload className="text-3xl mb-2" />
+                <span>Upload Image</span>
+              </label>
+              <input
+                id="imageUpload"
+                type="file"
+                accept="image/*"
+                className="hidden"
+                onChange={handleImageUpload}
+              />
             </div>
-            <span>{uploadedImage ? "Change Image" : "Cover Image"}</span>
-          </label>
-          <input
-            id="imageUpload"
-            type="file"
-            accept="image/*"
-            className="hidden"
-            onChange={handleImageUpload}
-          />
+          )}
         </div>
+        {uploadedImage && (
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg w-full mb-8"
+            onClick={() => setUploadedImage(null)}
+          >
+            Change Image
+          </button>
+        )}
         <div className="mb-4">
           <label htmlFor="name" className="text-xl">
             Story Title
@@ -66,34 +92,46 @@ const CreatePage = () => {
           <textarea
             id="description"
             className="border-2 w-full pl-2 py-2"
-            rows={4}
+            rows={2}
             onChange={(e) => setDescription(String(e.target.value))}
           ></textarea>
         </div>
-        <div className="border-2 rounded-lg p-8 mb-8 flex flex-col items-center">
-          <label
-            htmlFor="audioUpload"
-            className="cursor-pointer flex flex-col items-center"
-          >
-            <FaUpload className="text-3xl mb-2" />
-            <span>Add Audio File</span>
-          </label>
+        <div className="border-2 rounded-lg p-8 mb-8 flex flex-col items-center justify-center">
+          {selectedAudio ? (
+            <div className="flex flex-col items-center">
+              <span className="text-lg mb-2">{selectedAudio.name}</span>
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg"
+                onClick={() => setSelectedAudio(null)}
+              >
+                Remove Audio
+              </button>
+            </div>
+          ) : (
+            <label htmlFor="audioUpload" className="cursor-pointer flex flex-col items-center">
+              <FaUpload className="text-3xl mb-2" />
+              <span>Add Audio File</span>
+            </label>
+          )}
           <input
             id="audioUpload"
             type="file"
             accept="audio/*"
             className="hidden"
+            onChange={handleAudioUpload}
           />
         </div>
         <div className="text-center">
           <button
             type="button"
             className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg w-full"
+            onClick={handleSubmit}
           >
             Submit
           </button>
         </div>
       </div>
+      {showPopUp && <QrPopUp onClose={handleClosePopUp} data={""} onBack={() => {}} />}
     </div>
   );
 };
