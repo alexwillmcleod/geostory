3e08ed81cc312cc79e8864fa728a2060000528b9
Alex McLeod <alexwillmcleod@gmail.com>
2023-07-09 12:35:15 +1200

    refactor: updated story route to use id

--- api/dist/routes/story.js
+++ api/dist/routes/story.js
@@ -2,9 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const client_s3_1 = require("@aws-sdk/client-s3");
@@ -53,14 +50,23 @@ exports.router.post('/:name/:description', upload.single('audio'), auth_1.defaul
     catch (err) {
         console.error(err);
     }
-    return res.status(200).send(fileUrl);
+    return res.status(200).send(storyDoc._id);
 });
 exports.router.get('/:id', async (req, res) => {
-    const { id } = req.query;
+    const { id } = req.params;
     if (!id)
-        return res.status(400).send('`id` is a required query');
-    const foundStory = await schema_1.Story.findOne({ audio: id });
+        return res.status(400).send('`id` is a required param');
+    const foundStory = await schema_1.Story.findById(id);
     if (!foundStory)
         return res.status(400).send('could not find story with that id');
-    return res.status(200).json(foundStory);
+    const author = await schema_1.User.findById(foundStory.author);
+    if (!author)
+        return res.status(500).send('could not find author for post');
+    const story = {
+        name: foundStory.name,
+        authorName: author.name,
+        description: foundStory.description,
+        audio: foundStory.audio,
+    };
+    return res.status(200).json(story);
 });
--- api/routes/story.ts
+++ api/routes/story.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { Request, Router, Response } from 'express';
 import auth from '../middleware/auth';
 import { v4 as uuidv4 } from 'uuid';
-import { Story } from '../db/schema';
+import { Story, User } from '../db/schema';
 import multer from 'multer';
 import fs from 'fs';
 
@@ -58,15 +58,26 @@ router.post(
       console.error(err);
     }
 
-    return res.status(200).send(fileUrl);
+    return res.status(200).send(storyDoc._id);
   }
 );
 
 router.get('/:id', async (req: Request, res: Response) => {
-  const { id } = req.query;
-  if (!id) return res.status(400).send('`id` is a required query');
-  const foundStory = await Story.findOne({ audio: id });
+  const { id } = req.params;
+  if (!id) return res.status(400).send('`id` is a required param');
+  const foundStory = await Story.findById(id);
   if (!foundStory)
     return res.status(400).send('could not find story with that id');
-  return res.status(200).json(foundStory);
+
+  const author = await User.findById(foundStory.author);
+  if (!author) return res.status(500).send('could not find author for post');
+
+  const story = {
+    name: foundStory.name,
+    authorName: author.name,
+    description: foundStory.description,
+    audio: foundStory.audio,
+  };
+
+  return res.status(200).json(story);
 });
